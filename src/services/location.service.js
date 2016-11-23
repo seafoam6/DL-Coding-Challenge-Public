e70625fa7a6a680a1d81b72d3b6869e2cc40f352
Steve Barman <steve@stevebarman.com>
2016-11-23 15:03:46 -0500

    Get city using Google API

--- src/services/location.service.js
+++ src/services/location.service.js
@@ -1,5 +1,14 @@
 function locationService($http,  googleLocationKey,  $log){
 
+  //for testing complex city names
+  // coordinates for Sault St. Marie
+  var sault = {
+    latitude:46.52185799999999,
+    longitude:-84.346089600000
+  }
+
+  //private methods
+
   function getUrl(coordinates){
     return `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coordinates.latitude},${coordinates.longitude}&key=${googleLocationKey}`
   }
@@ -7,28 +16,31 @@ function locationService($http,  googleLocationKey,  $log){
 
   // public methods
 
-  this.getCityState = () => {
-
+  this.getCity = () => {
     getCoordinates()
       .then((coordinates) =>{
-
         $http({
           method:'GET',
           url:getUrl(coordinates),
           cache:true
-        }).then((data) => console.log('FROM GOOGLE', data))
+        }).then((data) => {
+          //extract City Name
+          let cityNameRaw = _.get(data, 'data.results[1].address_components[0].long_name');
+          // format city for weather underground
+          let cityNameFormatted = cityNameRaw.toLowerCase().replace(/ /g, '_');
+          //console.log(cityNameFormatted)
+          return cityNameFormatted;
+        })
 
       })
       .catch((err) => console.log(err))
   }
 
-
-
-  // private methods
-  function getCoordinates(){
+  this.getCoordinates = () => {
+    console.log('ugh')
     return new Promise(function(resolve, reject){
       navigator.geolocation.getCurrentPosition(function(position) {
-        console.log(position)
+        //console.log(position)
         resolve({
           latitude:position.coords.latitude,
           longitude:position.coords.longitude,
