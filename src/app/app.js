bcb789c0ab74c0dd2cc280807500820dfb77f3c6
Steve Barman <steve@stevebarman.com>
2016-11-23 10:41:05 -0500

    adds favicon and stylesheet

--- src/app/app.js
+++ src/app/app.js
@@ -1,17 +1,18 @@
-
+// 3rd party
 import angular from 'angular';
 import uirouter from  'angular-ui-router';
 import localStorageService from 'angular-local-storage';
 import ngAnimate from 'angular-animate';
 import angularMaterial from 'angular-material';
+// utilities
+import moment from 'moment';
+import _ from 'lodash';
 
 
 // Styles + fonts
 require('../../node_modules/angular-material/angular-material.css');
+require('!style!css!sass!../styles/index.scss');
 
-// utilities
-import moment from 'moment';
-import _ from 'lodash';
 
 // app
 let app = () => {
@@ -33,7 +34,7 @@ angular.module('dl', [localStorageService, uirouter, ngAnimate, angularMaterial
 
 })
 
-// gets user agent for easier debugging on devices
+// gets user agent for easier debugging weird quirks
 var doc = document.documentElement;
 doc.setAttribute('data-useragent', navigator.userAgent);
 
